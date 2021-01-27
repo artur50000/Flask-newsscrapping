3d9055d69320f02d5f3f4558a3a5deea1203f6dd
artur50000 <artur50000@gmail.com>
2021-01-27 22:43:47 +0300

    changes

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+
 SECRET_KEY='topsecret'
-SQLALCHEMY_DATABASE_URI='postgresql://postgres:123456@localhost/news'
+#SQLALCHEMY_DATABASE_URI='postgresql://postgres:123456@localhost/news'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS=False
